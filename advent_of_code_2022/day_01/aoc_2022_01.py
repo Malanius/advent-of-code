74a8399b60440e1173a96fb856c65e738f91617a
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-01 09:46:44 +0100

    (day-01): Solve part 1

---
+++ advent_of_code_2022/day_01/aoc_2022_01.py
@@ -0,0 +1,49 @@
+import pathlib
+
+from advent_of_code_2022.util.perf import perf
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+
+def parse(puzzle_input: str) -> str:
+    """Parse input"""
+    return puzzle_input
+
+
+def count_calories_per_elf(data: str) -> list[int]:
+    """Count calories per elf"""
+    totals: list[int] = []
+    carried: int = 0
+    for line in data.splitlines():
+        if line == "":
+            totals.append(carried)
+            carried = 0
+        else:
+            carried += int(line)
+    return totals
+
+
+@perf
+def part1(data: str) -> int:
+    """Find out how many calories top elf has"""
+    totals = count_calories_per_elf(data)
+    return max(totals)
+
+
+@perf
+def part2(data):
+    """Solve part 2"""
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input"""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    puzzle_input = (PUZZLE_DIR / "data.txt").read_text().strip()
+    solutions = solve(puzzle_input)
+    print("\n".join(str(solution) for solution in solutions))
