3c0b010fc9ecf87124de612cba789990524e18e1
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-05 09:37:29 +0100

    (day-05): Parse moves input

--- advent_of_code_2022/day_05/aoc_2022_05.py
+++ advent_of_code_2022/day_05/aoc_2022_05.py
@@ -1,33 +1,58 @@
 import pathlib
+import re
+
+from attr import dataclass
 
 from advent_of_code_2022.util.perf import perf
 
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
 
-def parse(puzzle_input):
-    """Parse input"""
+@dataclass
+class Move:
+    count: int
+    source: int
+    target: int
+
+
+COMMAND_PATTERN = re.compile(r"move (\d+) from (\d+) to (\d+)")
+
+
+def parse_init(puzzle_input):
+    """Parse initial state"""
+
+
+def parse_moves(moves_input: str) -> list[Move]:
+    """Parse input commands"""
+    commands = []
+    for line in moves_input.splitlines():
+        match = COMMAND_PATTERN.match(line)
+        if match:
+            commands.append(Move(*map(int, match.groups())))
+    return commands
 
 
 @perf
-def part1(data):
+def part1(init, moves):
     """Solve part 1"""
 
 
 @perf
-def part2(data):
+def part2(init, moves):
     """Solve part 2"""
 
 
-def solve(puzzle_input):
+def solve(init_input, moves_input):
     """Solve the puzzle for the given input"""
-    data = parse(puzzle_input)
-    solution1 = part1(data)
-    solution2 = part2(data)
+    init_data = parse_init(init_input)
+    moves = parse_moves(moves_input)
+    solution1 = part1(init_data, moves)
+    solution2 = part2(init_data, moves)
     return solution1, solution2
 
 
 if __name__ == "__main__":
-    puzzle_input = (PUZZLE_DIR / "data.txt").read_text().strip()
-    solutions = solve(puzzle_input)
+    init_input = (PUZZLE_DIR / "data-init.csv").read_text().strip()
+    moves_input = (PUZZLE_DIR / "data-moves.txt").read_text().strip()
+    solutions = solve(init_input, moves_input)
     print("\n".join(str(solution) for solution in solutions))
--- advent_of_code_2022/day_05/test_aoc_2022_05.py
+++ advent_of_code_2022/day_05/test_aoc_2022_05.py
@@ -6,30 +6,40 @@ PUZZLE_DIR = pathlib.Path(__file__).parent
 
 
 @pytest.fixture
-def example1():
-    puzzle_input = (PUZZLE_DIR / "example-1.txt").read_text().strip()
-    return solver.parse(puzzle_input)
+def example_init():
+    puzzle_input = (PUZZLE_DIR / "example-init.csv").read_text().strip()
+    return solver.parse_init(puzzle_input)
 
 
 @pytest.fixture
-def example2():
-    puzzle_input = (PUZZLE_DIR / "example-2.txt").read_text().strip()
-    return solver.parse(puzzle_input)
+def example_moves():
+    puzzle_input = (PUZZLE_DIR / "example-moves.txt").read_text().strip()
+    return solver.parse_moves(puzzle_input)
 
 
 @pytest.mark.skip(reason="Not implemented")
-def test_parse_example1(example1):
-    """Test that input is parsed properly"""
-    assert example1 == ...
+def test_parse_init(example_init):
+    """Test that init input is parsed properly"""
+    assert example_init == ...
+
+
+def test_parse_moves(example_moves):
+    """Test that moves input is parsed properly"""
+    assert example_moves == [
+        solver.Move(1, 2, 1),
+        solver.Move(3, 1, 3),
+        solver.Move(2, 2, 1),
+        solver.Move(1, 1, 2),
+    ]
 
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part1_example1(example1):
+def test_part1_example(example_init, example_moves):
     """Test part 1 on example input"""
-    assert solver.part1(example1) == ...
+    assert solver.part1(example_init,example_moves) == ...
 
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example2):
+def test_part2_example(example_init, example_moves):
     """Test part 2 on example input"""
-    assert solver.part2(example2) == ...
+    assert solver.part2(example_init, example_moves) == ...
