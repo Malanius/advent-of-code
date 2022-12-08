46ba55d319c28b409cdf2fe43484739c7e502231
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-08 19:30:40 +0100

    (perf): Report runtime in ms

--- advent_of_code_2022/util/perf.py
+++ advent_of_code_2022/util/perf.py
@@ -1,14 +1,14 @@
 from functools import wraps
-from time import perf_counter
+from time import perf_counter_ns
 
 
 def perf(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        start = perf_counter()
+        start = perf_counter_ns()
         result = func(*args, **kwargs)
-        end = perf_counter()
-        print(f"{func.__name__} took {(end - start):0.4f} seconds")
+        end = perf_counter_ns()
+        print(f"{func.__name__} took {(end - start)/1_000_000:0.4f} ms")
         return result
 
     return wrapper
