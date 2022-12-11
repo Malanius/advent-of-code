0a17b92e489592ddb36cdb0c1cfb3b39b44312a5
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-11 09:53:14 +0100

    (day-11): Parse puzzle input

--- advent_of_code_2022/day_11/aoc_2022_11.py
+++ advent_of_code_2022/day_11/aoc_2022_11.py
@@ -1,12 +1,58 @@
+import logging
 import pathlib
+from collections import deque
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
 
 from advent_of_code_2022.util.perf import perf
 
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
+logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
-def parse(puzzle_input):
+
+@dataclass
+class Monkey:
+    id: int
+    items: Optional[deque] = None
+    operation: Optional[str] = None
+    test_divisible_by: Optional[int] = None
+    test_true_target: Optional[int] = None
+    test_false_target: Optional[int] = None
+
+
+def parse(puzzle_input: str) -> dict[int, Monkey]:
     """Parse input"""
+    lines = puzzle_input.splitlines()
+    monkeys = {}
+    current_monkey = 0
+    for line in lines:
+        match line.split():
+            case ["Monkey", id]:
+                current_monkey = int(id[:-1])  # remove colon
+                monkeys[current_monkey] = Monkey(current_monkey)
+                logging.debug(f"Created new monkey {current_monkey}:")
+            case ["Starting", "items:", *items]:
+                items = "".join(items).split(",")
+                items = deque(int(item) for item in items)
+                monkeys[current_monkey].items = items
+                logging.debug(f"  Monkey {current_monkey} has items {items}")
+            case ["Operation:", *operation]:
+                operation = " ".join(operation)
+                monkeys[current_monkey].operation = operation
+                logging.debug(f"  Monkey {current_monkey} has operation: {operation}")
+            case ["Test:", "divisible", "by", n]:
+                monkeys[current_monkey].test_divisible_by = int(n)
+                logging.debug(f"  Monkey {current_monkey} tests divisible by {n}:")
+            case ["If", "true:", "throw", "to", "monkey", n]:
+                monkeys[current_monkey].test_true_target = int(n)
+                logging.debug(f"    Monkey {current_monkey} tests true throws to {n}")
+            case ["If", "false:", "throw", "to", "monkey", n]:
+                monkeys[current_monkey].test_false_target = int(n)
+                logging.debug(f"    Monkey {current_monkey} tests false throws to {n}")
+    logging.debug(monkeys)
+    return monkeys
 
 
 @perf
--- advent_of_code_2022/day_11/test_aoc_2022_11.py
+++ advent_of_code_2022/day_11/test_aoc_2022_11.py
@@ -1,3 +1,4 @@
+from collections import deque
 import pathlib
 import pytest
 import aoc_2022_11 as solver
@@ -6,8 +7,8 @@ PUZZLE_DIR = pathlib.Path(__file__).parent
 
 
 @pytest.fixture
-def example1():
-    puzzle_input = (PUZZLE_DIR / "example-1.txt").read_text().strip()
+def example():
+    puzzle_input = (PUZZLE_DIR / "example.txt").read_text().strip()
     return solver.parse(puzzle_input)
 
 
@@ -17,10 +18,51 @@ def example2():
     return solver.parse(puzzle_input)
 
 
-@pytest.mark.skip(reason="Not implemented")
-def test_parse_example1(example1):
+def test_parse_example(example):
     """Test that input is parsed properly"""
-    assert example1 == ...
+    assert len(example) == 4, "Should have 4 monkeys"
+
+    monkey0 = example[0]
+    assert monkey0.id == 0, "Monkey 0 should have id 0"
+    assert monkey0.items == deque([79, 98]), "Monkey 0 should have items 79, 98"
+    assert (
+        monkey0.operation == "new = old * 19"
+    ), "Monkey 0 should have operation new = old * 19"
+    assert monkey0.test_divisible_by == 23, "Monkey 0 should test divisible by 23"
+    assert monkey0.test_true_target == 2, "Monkey 0 should throw to monkey 2 if true"
+    assert monkey0.test_false_target == 3, "Monkey 0 should throw to monkey 3 if false"
+
+    monkey1 = example[1]
+    assert monkey1.id == 1, "Monkey 1 should have id 1"
+    assert monkey1.items == deque(
+        [54, 65, 75, 74]
+    ), "Monkey 1 should have items 54, 65, 75, 74"
+    assert (
+        monkey1.operation == "new = old + 6"
+    ), "Monkey 1 should have operation new = old + 6"
+    assert monkey1.test_divisible_by == 19, "Monkey 1 should test divisible by 19"
+    assert monkey1.test_true_target == 2, "Monkey 1 should throw to monkey 2 if true"
+    assert monkey1.test_false_target == 0, "Monkey 1 should throw to monkey 0 if false"
+
+    monkey2 = example[2]
+    assert monkey2.id == 2, "Monkey 2 should have id 2"
+    assert monkey2.items == deque([79, 60, 97]), "Monkey 2 should have items 79, 60, 97"
+    assert (
+        monkey2.operation == "new = old * old"
+    ), "Monkey 2 should have operation new = old * old"
+    assert monkey2.test_divisible_by == 13, "Monkey 2 should test divisible by 13"
+    assert monkey2.test_true_target == 1, "Monkey 2 should throw to monkey 1 if true"
+    assert monkey2.test_false_target == 3, "Monkey 2 should throw to monkey 3 if false"
+
+    monkey3 = example[3]
+    assert monkey3.id == 3, "Monkey 3 should have id 3"
+    assert monkey3.items == deque([74]), "Monkey 3 should have items 74"
+    assert (
+        monkey3.operation == "new = old + 3"
+    ), "Monkey 3 should have operation new = old + 3"
+    assert monkey3.test_divisible_by == 17, "Monkey 3 should test divisible by 17"
+    assert monkey3.test_true_target == 0, "Monkey 3 should throw to monkey 0 if true"
+    assert monkey3.test_false_target == 1, "Monkey 3 should throw to monkey 1 if false"
 
 
 @pytest.mark.skip(reason="Not implemented")
