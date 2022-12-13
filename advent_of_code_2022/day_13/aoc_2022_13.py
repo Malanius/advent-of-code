03f1e69fa64025d66a522542558c9d1e7704d215
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-13 19:01:03 +0100

    (day-13): Handle all 3 possible outcomes correctly
    Turn out, just two states are not enough for the comparison, when the elements are equal, we need to continue looking at others.

--- advent_of_code_2022/day_13/aoc_2022_13.py
+++ advent_of_code_2022/day_13/aoc_2022_13.py
@@ -1,9 +1,10 @@
-from collections import namedtuple
-from dataclasses import dataclass
-from itertools import zip_longest
 import json
 import logging
 import pathlib
+from collections import namedtuple
+from dataclasses import dataclass
+from enum import Enum
+from itertools import zip_longest
 from typing import Generator
 
 from advent_of_code_2022.util.perf import perf
@@ -12,6 +13,14 @@ PUZZLE_DIR = pathlib.Path(__file__).parent
 logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
 Pair = namedtuple("Pair", ["left", "right"])
+
+
+class Check(Enum):
+    """Comparison outcome"""
+
+    WRONG = -1
+    CONTINUE = 0
+    OK = 1
 
 
 def parse(puzzle_input: str) -> Generator[Pair, None, None]:
@@ -25,16 +34,18 @@ def parse(puzzle_input: str) -> Generator[Pair, None, None]:
         yield Pair(left, right)
 
 
-def is_ordered_correctly(pair: Pair) -> bool:
+def is_ordered_correctly(pair: Pair) -> Check:
     """Check if the pair is ordered correctly"""
     logging.debug(f"Checking pair: {pair}")
-    left = pair.left
-    right = pair.right
+    left, right = pair
 
     if isinstance(left, int) and isinstance(right, int):
-        result = left < right
-        logging.debug(f"Pair is ordered correctly: {result}")
-        return result
+        if left < right:
+            return Check.OK
+        if left == right:
+            return Check.CONTINUE
+        if left > right:
+            return Check.WRONG
 
     if isinstance(left, list) and isinstance(right, int):
         pair = Pair(left, [right])
@@ -48,13 +59,15 @@ def is_ordered_correctly(pair: Pair) -> bool:
         for left_part, right_part in zip_longest(left, right):
             if left_part is None:
                 logging.debug(f"Left side ran out of items, right order.")
-                return True
+                return Check.OK
             if right_part is None:
                 logging.debug(f"Right side ran out of items, not right order.")
-                return False
-            if is_ordered_correctly(Pair(left_part, right_part)):
-                return True
-        return False
+                return Check.WRONG
+            check = is_ordered_correctly(Pair(left_part, right_part))
+            if check != Check.CONTINUE:
+                logging.debug(f"Comparison outcome: {check}")
+                return check
+        return Check.CONTINUE
 
     raise ValueError(f"Unexpected pair: {pair}")
 
@@ -65,7 +78,7 @@ def part1(data: Generator[Pair, None, None]) -> int:
     correct_pairs = []
     for index, pair in enumerate(data, start=1):
         logging.debug(f"== Pair {index} ==")
-        if is_ordered_correctly(pair):
+        if is_ordered_correctly(pair) == Check.OK:
             logging.debug(f"Pair {index} is ordered correctly.")
             correct_pairs.append(index)
         else:
