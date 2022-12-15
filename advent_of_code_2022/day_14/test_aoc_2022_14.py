f570fcecbaf44c25069e4ece541a2b425314f948
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-15 11:10:17 +0100

    (day-14): Solve part 1

--- advent_of_code_2022/day_14/test_aoc_2022_14.py
+++ advent_of_code_2022/day_14/test_aoc_2022_14.py
@@ -15,6 +15,12 @@ def sample_grid() -> Grid:
 
 
 @pytest.fixture
+def data_grid() -> Grid:
+    puzzle_input = (PUZZLE_DIR / "data.txt").read_text().strip()
+    return solver.parse(puzzle_input)
+
+
+@pytest.fixture
 def example2():
     puzzle_input = (PUZZLE_DIR / "example-2.txt").read_text().strip()
     return solver.parse(puzzle_input)
@@ -23,6 +29,11 @@ def example2():
 def test_part1_example1(sample_grid):
     """Test part 1 on example input"""
     assert solver.part1(sample_grid) == 24
+
+
+def test_part1_data(data_grid):
+    """Test part 1 on example input"""
+    assert solver.part1(data_grid) == 665
 
 
 @pytest.mark.skip(reason="Not implemented")
