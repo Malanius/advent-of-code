eb96b1372e4aba52fc66b84e4af98a02b6784bf8
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-15 11:18:37 +0100

    (day-14): Add args flags for interactive, run on data and verbose

--- advent_of_code_2022/day_14/aoc_2022_14.py
+++ advent_of_code_2022/day_14/aoc_2022_14.py
@@ -1,7 +1,9 @@
+import argparse
 import logging
 import pathlib
 from copy import deepcopy
 
+from advent_of_code_2022.day_14.arguments import init_args
 from advent_of_code_2022.day_14.grid import Grid
 from advent_of_code_2022.day_14.simulation import Simulation
 from advent_of_code_2022.util.perf import perf
@@ -26,20 +28,25 @@ def part1(grid: Grid, interactive: bool = False):
 
 
 @perf
-def part2(data):
+def part2(grid: Grid, interactive: bool = False) -> int:
     """Solve part 2"""
 
 
-def solve(puzzle_input):
+def solve(puzzle_input: str, interactive_part: int | None) -> tuple[int, int]:
     """Solve the puzzle for the given input"""
-    interactive = True
-    data = parse(puzzle_input, interactive)
-    solution1 = part1(deepcopy(data), interactive)
-    solution2 = part2(deepcopy(data))
+    data = parse(puzzle_input, interactive_part is not None)
+    solution1 = part1(deepcopy(data), interactive_part == 1)
+    solution2 = part2(deepcopy(data), interactive_part == 2)
     return solution1, solution2
 
 
 if __name__ == "__main__":
-    puzzle_input = (PUZZLE_DIR / "example.txt").read_text().strip()
-    solutions = solve(puzzle_input)
+    args = init_args()
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    data_file = "example.txt" if not args.data else "data.txt"
+    puzzle_input = (PUZZLE_DIR / data_file).read_text().strip()
+
+    solutions = solve(puzzle_input, args.interactive)
     print("\n".join(str(solution) for solution in solutions))
