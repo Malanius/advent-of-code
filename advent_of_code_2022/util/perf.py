8c61dfcb3328214e011284e103acc149f3cd932e
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-18 15:01:13 +0100

    (util): Report longer running times in more sensible units

--- advent_of_code_2022/util/perf.py
+++ advent_of_code_2022/util/perf.py
@@ -8,7 +8,16 @@ def perf(func):
         start = perf_counter_ns()
         result = func(*args, **kwargs)
         end = perf_counter_ns()
-        print(f"{func.__name__} took {(end - start)/1_000_000:0.4f} ms")
+
+        time_taken = (end - start) / 1_000_000
+        took = f"{time_taken:0.4f} ms"
+        if time_taken > 1_000:
+            seconds = time_taken / 1_000
+            took = f"{seconds:0.4f} s"
+        if time_taken > 1_000 * 60:
+            mins = time_taken / (1_000 * 60)
+            took = f"{mins:0.4f} min"
+        print(f"{func.__name__} took {took}")
         return result
 
     return wrapper
