9f7eed6b3a02820b419e7596381e47fac68ee4b0
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-19 07:45:15 +0100

    (template): Update test tempalte

--- advent_of_code_2022/templates/test_solver.py
+++ advent_of_code_2022/templates/test_solver.py
@@ -6,30 +6,41 @@ PUZZLE_DIR = pathlib.Path(__file__).parent
 
 
 @pytest.fixture
-def example1():
-    puzzle_input = (PUZZLE_DIR / "example-1.txt").read_text().strip()
+def example():
+    puzzle_input = (PUZZLE_DIR / "example.txt").read_text().strip()
     return solver.parse(puzzle_input)
 
 
 @pytest.fixture
-def example2():
-    puzzle_input = (PUZZLE_DIR / "example-2.txt").read_text().strip()
+def data():
+    puzzle_input = (PUZZLE_DIR / "data.txt").read_text().strip()
     return solver.parse(puzzle_input)
 
 
 @pytest.mark.skip(reason="Not implemented")
-def test_parse_example1(example1):
+def test_parse_example(example):
     """Test that input is parsed properly"""
-    assert example1 == ...
+    assert example == ...
 
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part1_example1(example1):
+def test_part1_example(example):
     """Test part 1 on example input"""
-    assert solver.part1(example1) == ...
+    assert solver.part1(example) == ...
 
 
 @pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example2):
+def test_part1_data(data):
+    """Test part 1 on data input"""
+    assert solver.part1(data) == ...
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example(example):
     """Test part 2 on example input"""
-    assert solver.part2(example2) == ...
+    assert solver.part2(example) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_data(data):
+    """Test part 2 on data input"""
+    assert solver.part2(data) == ...
