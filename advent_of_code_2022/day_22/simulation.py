7d4617b63ed7294845e8b8542072a496ccf0724b
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-22 21:21:16 +0100

    (day-22): Fix marked path deleting edges from tiles

--- advent_of_code_2022/day_22/simulation.py
+++ advent_of_code_2022/day_22/simulation.py
@@ -29,8 +29,11 @@ class Simulation:
 
     def _mark_passed(self) -> None:
         y, x = self.player.coords()
-        passed_air = Tile(passed=True, passed_direction=self.player.facing)
-        self.grid.grid[y][x] = passed_air
+        passed_tile = self.grid.grid[y][x]
+        if not isinstance(passed_tile, Tile):
+            raise ValueError("Player path not on tile!")
+        passed_tile.passed = True
+        passed_tile.passed_direction = self.player.facing
         logging.debug(f"Marked {x}, {y} as visited")
 
     def _print_state(self, clear: bool = True, partial: bool = True) -> None:
@@ -50,39 +53,44 @@ class Simulation:
             return
         self.player.turn(turn)
         logging.debug(f"Rotated player to {self.player.facing}")
+        logging.debug(
+            f"Player at: {self.player.coords}, on: {self.player.standing_on!r}"
+        )
         self._print_state()
 
     def _move_player(self, steps) -> None:
         for _ in range(steps):
-            if self.interactive:
-                new_coords = self.player.coords + Coord(*self.player.facing.value)
+            new_coords = self.player.coords + Coord(*self.player.facing.value)
 
-                current_element = self.player.standing_on
-                if not isinstance(current_element, Tile):
-                    raise ValueError(f"Player is not on air! {current_element!r}")
+            current_element = self.player.standing_on
+            if not isinstance(current_element, Tile):
+                raise ValueError(f"Player is not on air! {current_element!r}")
 
-                if current_element.edges:
-                    current_coords = self.player.coords
-                    self.grid.grid[current_coords.y][
-                        current_coords.x
-                    ] = self.player.standing_on
-                    new_coords = self.grid.wraps_to(current_coords, self.player.facing)
-                    self.grid.grid[current_coords.y][current_coords.x] = self.player
-                    logging.debug(f"Wrapping to {new_coords}")
+            if current_element.edges:
+                current_coords = self.player.coords
+                self.grid.grid[current_coords.y][
+                    current_coords.x
+                ] = self.player.standing_on
+                new_coords = self.grid.wraps_to(current_coords, self.player.facing)
+                self.grid.grid[current_coords.y][current_coords.x] = self.player
+                logging.debug(f"Wrapping to {new_coords}")
 
-                element = self.grid.grid[new_coords.y][new_coords.x]
-                if not self.player.can_move_to(element):
-                    logging.debug(f"Can't move to '{element}' at {new_coords}")
-                    return
+            element = self.grid.grid[new_coords.y][new_coords.x]
+            if not self.player.can_move_to(element):
+                logging.debug(f"Can't move to '{element}' at {new_coords}")
+                return
 
-                self._mark_passed()
-                self.player.standing_on = self.grid.grid[new_coords.y][new_coords.x]
-                self.grid.grid[new_coords.y][new_coords.x] = self.player
-                self.player.move_to(new_coords)
-                logging.debug(
-                    f"Player at: {self.player.coords}, on: {self.player.standing_on!r}"
-                )
-                self._print_state()
+            self.grid.grid[self.player.coords.y][
+                self.player.coords.x
+            ] = self.player.standing_on
+            self.player.standing_on = self.grid.grid[new_coords.y][new_coords.x]
+            self._mark_passed()
+            self.grid.grid[new_coords.y][new_coords.x] = self.player
+            self.player.move_to(new_coords)
+            logging.debug(
+                f"Player at: {self.player.coords}, on: {self.player.standing_on!r}"
+            )
+            self._print_state()
 
     def run(self) -> None:
         if self.interactive:
