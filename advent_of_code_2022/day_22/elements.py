a23ef9eaa90e1ffda1e67d9ca54f9e94b322c3a8
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-22 15:40:41 +0100

    (day-22): Extract player to own file

--- advent_of_code_2022/day_22/elements.py
+++ advent_of_code_2022/day_22/elements.py
@@ -11,7 +11,7 @@ class Element:
 class Void(Element):
     def __str__(self) -> str:
         return " "
-        return "\u2588" # Full block for debugging
+        return "\u2588"  # Full block for debugging
 
 
 class Rock(Element):
@@ -36,40 +36,3 @@ class Air(Element):
                 return "<"
             case Direction.RIGHT:
                 return ">"
-
-
-@dataclass
-class Player(Element):
-    coords: tuple[int, int]
-    facing: Direction = Direction.RIGHT
-
-    def __str__(self) -> str:
-        return "*"
-
-    def can_move_to(self, element: Element) -> bool:
-        if isinstance(element, Air):
-            return True
-        return False
-
-    def move(self, direction: Direction):
-        x, y = self.coords
-        self.coords = (x + direction.value[0], y + direction.value[1])
-
-    def turn(self, to: Literal["L", "R"]):
-        match to, self.facing:
-            case ("L", Direction.UP):
-                self.facing = Direction.LEFT
-            case ("L", Direction.LEFT):
-                self.facing = Direction.DOWN
-            case ("L", Direction.DOWN):
-                self.facing = Direction.RIGHT
-            case ("L", Direction.RIGHT):
-                self.facing = Direction.UP
-            case ("R", Direction.UP):
-                self.facing = Direction.RIGHT
-            case ("R", Direction.RIGHT):
-                self.facing = Direction.DOWN
-            case ("R", Direction.DOWN):
-                self.facing = Direction.LEFT
-            case ("R", Direction.LEFT):
-                self.facing = Direction.UP
