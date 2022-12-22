138959009b09004ea58279b6b1216928e7dbd89c
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-22 15:39:13 +0100

    (day-22): Add score for direction

--- advent_of_code_2022/day_22/direction.py
+++ advent_of_code_2022/day_22/direction.py
@@ -2,7 +2,19 @@ from enum import Enum
 
 
 class Direction(Enum):
-    UP = (-1, 0)
+    RIGHT = (0, 1)
     DOWN = (1, 0)
     LEFT = (0, -1)
-    RIGHT = (0, 1)
+    UP = (-1, 0)
+
+    @property
+    def score(self) -> int:
+        match self:
+            case Direction.RIGHT:
+                return 0
+            case Direction.DOWN:
+                return 1
+            case Direction.LEFT:
+                return 2
+            case Direction.UP:
+                return 3
