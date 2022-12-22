96657d0900eafcb977d15c171f6ff0c1f7c57538
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-22 13:16:15 +0100

    (day-22): Parse instructions

--- advent_of_code_2022/day_22/aoc_2022_22.py
+++ advent_of_code_2022/day_22/aoc_2022_22.py
@@ -3,23 +3,31 @@ import pathlib
 from typing import Optional
 
 from advent_of_code_2022.day_22.arguments import init_args
+from advent_of_code_2022.day_22.grid import Grid
+from advent_of_code_2022.day_22.instructions import Instruction, parse_instructions
 from advent_of_code_2022.util.perf import perf
 
 PUZZLE_DIR = pathlib.Path(__file__).parent
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
+Data = tuple[Grid, list[Instruction]]
 
-def parse(puzzle_input):
+
+def parse(puzzle_input: str) -> Data:
     """Parse input"""
+    parts = puzzle_input.split("\n\n")
+    grid = Grid.construct(parts[0])
+    instructions = parse_instructions(parts[1])
+    return grid, instructions
 
 
 @perf
-def part1(data, interactive: bool = False):
+def part1(data: Data, interactive: bool = False):
     """Solve part 1"""
 
 
 @perf
-def part2(data, interactive: bool = False):
+def part2(data: Data, interactive: bool = False):
     """Solve part 2"""
 
 
@@ -37,6 +45,6 @@ if __name__ == "__main__":
         logging.getLogger().setLevel(logging.DEBUG)
 
     data_file = "example.txt" if not args.data else "data.txt"
-    puzzle_input = (PUZZLE_DIR / data_file).read_text().strip()
+    puzzle_input = (PUZZLE_DIR / data_file).read_text()
     solutions = solve(puzzle_input, args.interactive)
     print("\n".join(str(solution) for solution in solutions))
