d4d92e074a688a4fb274197ccdc9ebc27cae661f
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-27 11:56:16 +0100

    (day-25): Convert DEC to SNAFU

--- advent_of_code_2022/day_25/converter.py
+++ advent_of_code_2022/day_25/converter.py
@@ -29,3 +29,29 @@ def snafu_to_dec(snafu: str) -> int:
     converted = sum(numbers)
     logging.debug(f"Converted {snafu} snf to {converted} dec")
     return converted
+
+
+def dec_to_snafu(dec: int) -> str:
+    snafu = []
+
+    while dec > 0:
+        remainder = dec % SNAFU_BASE
+
+        # we can't have a number greater than 2 in snafu
+        if remainder > 2:
+            remainder -= SNAFU_BASE  # so we subtract the base to get a negative number
+            dec += SNAFU_BASE  # and reflect that in remaining dec
+
+        match remainder:
+            case -2:
+                snafu.append("=")
+            case -1:
+                snafu.append("-")
+            case _:
+                snafu.append(str(remainder))
+
+        dec = dec // SNAFU_BASE
+
+    converted = "".join(reversed(snafu))
+    logging.debug(f"Converted {dec} dec to {converted} snf")
+    return converted
--- advent_of_code_2022/day_25/test_converter.py
+++ advent_of_code_2022/day_25/test_converter.py
@@ -1,6 +1,6 @@
 import pytest
 
-from advent_of_code_2022.day_25.converter import snafu_to_dec
+from advent_of_code_2022.day_25.converter import dec_to_snafu, snafu_to_dec
 
 
 @pytest.mark.parametrize(
@@ -21,5 +21,27 @@ from advent_of_code_2022.day_25.converter import snafu_to_dec
         ("122", 37),
     ],
 )
-def test_snafu_to_dec(input: str, expected: int):
+def test_snafu_to_dec(input: str, expected: int) -> None:
     assert snafu_to_dec(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        (1747, "1=-0-2"),
+        (906, "12111"),
+        (198, "2=0="),
+        (11, "21"),
+        (201, "2=01"),
+        (31, "111"),
+        (1257, "20012"),
+        (32, "112"),
+        (353, "1=-1="),
+        (107, "1-12"),
+        (7, "12"),
+        (3, "1="),
+        (37, "122"),
+    ],
+)
+def test_dec_to_snafu(input: int, expected: str) -> None:
+    assert dec_to_snafu(input) == expected
