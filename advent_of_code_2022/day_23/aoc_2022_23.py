72a9b8652a9b231a9f613dbcf5f02bdbf75049aa
Malanius Privierre <malaniusprivierre@gmail.com>
2022-12-28 11:12:34 +0100

    (day-23): Init day 23

---
+++ advent_of_code_2022/day_23/aoc_2022_23.py
@@ -0,0 +1,41 @@
+import logging
+import pathlib
+
+from advent_of_code_2022.day_23.arguments import init_args
+from advent_of_code_2022.util.perf import perf
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+
+def parse(puzzle_input):
+    """Parse input"""
+
+
+@perf
+def part1(data):
+    """Solve part 1"""
+
+
+@perf
+def part2(data):
+    """Solve part 2"""
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input"""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    args = init_args()
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    data_file = "example.txt" if not args.data else "data.txt"
+    puzzle_input = (PUZZLE_DIR / data_file).read_text().strip()
+    solutions = solve(puzzle_input)
+    print("\n".join(str(solution) for solution in solutions))
