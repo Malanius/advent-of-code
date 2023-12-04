d98df5173af70e2ab812be72246e5563733dd9f5
Malanius Privierre <malaniusprivierre@gmail.com>
2023-12-04 09:31:08 +0100

    chore(2023-04): init day

---
+++ advent_of_code/2023/day_04/test_aoc_2023_04.py
@@ -0,0 +1,46 @@
+import pathlib
+import pytest
+import aoc_2023_04 as solver
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+
+@pytest.fixture
+def example():
+    puzzle_input = (PUZZLE_DIR / "example.txt").read_text().strip()
+    return solver.parse(puzzle_input)
+
+
+@pytest.fixture
+def data():
+    puzzle_input = (PUZZLE_DIR / "data.txt").read_text().strip()
+    return solver.parse(puzzle_input)
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_parse_example(example):
+    """Test that input is parsed properly"""
+    assert example == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part1_example(example):
+    """Test part 1 on example input"""
+    assert solver.part1(example) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part1_data(data):
+    """Test part 1 on data input"""
+    assert solver.part1(data) == ...
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example(example):
+    """Test part 2 on example input"""
+    assert solver.part2(example) == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_data(data):
+    """Test part 2 on data input"""
+    assert solver.part2(data) == ...
