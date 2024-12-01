9db93fb8241342f293048a1839ee677e37601670
Malanius Privierre <malaniusprivierre@gmail.com>
2024-12-01 11:40:13 +0100

    feat(2024/01): init day

---
+++ advent_of_code/2024/day_01/arguments.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+def init_args() -> argparse.Namespace:
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--data",
+        required=False,
+        default=False,
+        action="store_true",
+        help="Use data.txt instead of example.txt",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        required=False,
+        default=False,
+        action="store_true",
+        help="Enable debug logging",
+    )
+    return parser.parse_args()
