61f24defb00bbf9ed18e80559d8fc667b2ae5734
Michal Slota <malaniusprivierre@gmail.com>
2025-12-16 19:16:06 +0100

    feat(2025/02): parse puzzle input

--- advent_of_code/y2025/day_02/src/lib.rs
+++ advent_of_code/y2025/day_02/src/lib.rs
@@ -1,11 +1,25 @@
-pub fn parse_input(puzzle_input: &str) -> Vec<isize> {
-    vec![]
+use std::ops::RangeInclusive;
+
+type PuzzleInput = Vec<RangeInclusive<isize>>;
+
+pub fn parse_input(puzzle_input: &str) -> PuzzleInput {
+    let ranges = puzzle_input.split(",");
+
+    let mut result: PuzzleInput = Vec::new();
+    for range_input in ranges {
+        let (start, end) = range_input.split_once("-").unwrap();
+        let start: isize = start.parse().unwrap();
+        let end: isize = end.parse().unwrap();
+        let range = start..=end;
+        result.push(range);
+    }
+    result
 }
 
-pub fn part1(data: &Vec<isize>) -> usize {
+pub fn part1(data: &PuzzleInput) -> usize {
     0
 }
 
-pub fn part2(data: &Vec<isize>) -> isize {
+pub fn part2(data: &PuzzleInput) -> isize {
     0
 }
--- advent_of_code/y2025/day_02/tests/tests.rs
+++ advent_of_code/y2025/day_02/tests/tests.rs
@@ -5,7 +5,22 @@ static DATA_INPUT: &str = include_str!("../data.txt");
 
 #[test]
 fn test_parse_example() {
-   panic!("not implemented"); 
+    assert_eq!(
+        parse_input(EXAMPLE_INPUT),
+        vec![
+            11..=22,
+            95..=115,
+            998..=1012,
+            1188511880..=1188511890,
+            222220..=222224,
+            1698522..=1698528,
+            446443..=446449,
+            38593856..=38593862,
+            565653..=565659,
+            824824821..=824824827,
+            2121212118..=2121212124
+        ]
+    );
 }
 
 // #[test]
