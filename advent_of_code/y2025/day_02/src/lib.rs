b224a509df8ee0cf0e9a916f53dbc21f409da4c1
Michal Slota <malaniusprivierre@gmail.com>
2025-12-16 21:09:32 +0100

    feat(2025/01): solve part 2, example ONLY

--- advent_of_code/y2025/day_02/src/lib.rs
+++ advent_of_code/y2025/day_02/src/lib.rs
@@ -16,13 +16,69 @@ pub fn parse_input(puzzle_input: &str) -> PuzzleInput {
     result
 }
 
-pub fn is_repeated_pattern(num: isize) -> bool {
+pub fn is_repeated_twice(num: isize) -> bool {
     let s = num.to_string();
     let len = s.len();
     (len % 2 == 0) && (s[..len / 2] == s[len / 2..])
 }
 
+pub fn is_repeated_pattern(num: isize) -> bool {
+    let s = num.to_string();
+
+    if is_repeated_twice(num) {
+        return true;
+    }
+
+    let counts = s
+        .chars()
+        .fold(std::collections::HashMap::new(), |mut acc, c| {
+            *acc.entry(c).or_insert(0) += 1;
+            acc
+        });
+
+    if counts.len() == 1 {
+        // all digits are the same
+        return true;
+    }
+
+    if counts.len() == s.len() {
+        // all digits are unique
+        return false;
+    }
+
+    let first_counted = counts.values().next().unwrap();
+    if !counts.values().all(|&count| count == *first_counted) {
+        return false;
+    }
+
+    let pattern_length = counts.len();
+    let pattern = &s[..pattern_length];
+    for i in 0..(s.len() / pattern_length) {
+        let start = i * pattern_length;
+        let end = start + pattern_length;
+        let current = &s[start..end];
+        if current != pattern {
+            return false;
+        }
+    }
+
+    true
+}
+
 pub fn part1(data: &PuzzleInput) -> isize {
+    let mut result = 0;
+
+    for range in data {
+        for num in range.clone() {
+            if is_repeated_twice(num) {
+                result += num;
+            }
+        }
+    }
+    result
+}
+
+pub fn part2(data: &PuzzleInput) -> isize {
     let mut result = 0;
 
     for range in data {
@@ -33,8 +89,4 @@ pub fn part1(data: &PuzzleInput) -> isize {
         }
     }
     result
-}
-
-pub fn part2(data: &PuzzleInput) -> isize {
-    0
 }
--- advent_of_code/y2025/day_02/tests/tests.rs
+++ advent_of_code/y2025/day_02/tests/tests.rs
@@ -1,4 +1,4 @@
-use aoc_2025_day_02::{is_repeated_pattern, parse_input, part1, part2};
+use aoc_2025_day_02::{is_repeated_pattern, is_repeated_twice, parse_input, part1, part2};
 
 static EXAMPLE_INPUT: &str = include_str!("../example.txt");
 static DATA_INPUT: &str = include_str!("../data.txt");
@@ -24,13 +24,176 @@ fn test_parse_example() {
 }
 
 #[test]
+fn test_is_repeated_twice() {
+    // examples that should return true
+    assert_eq!(is_repeated_twice(55), true);
+    assert_eq!(is_repeated_twice(6464), true);
+    assert_eq!(is_repeated_twice(123123), true);
+    // should be false
+    assert_eq!(is_repeated_twice(7), false);
+    assert_eq!(is_repeated_twice(111), false);
+    assert_eq!(is_repeated_twice(1234), false);
+}
+
+#[test]
 fn test_is_repeated_pattern() {
-    assert_eq!(is_repeated_pattern(55), true);
-    assert_eq!(is_repeated_pattern(6464), true);
-    assert_eq!(is_repeated_pattern(123123), true);
-    assert_eq!(is_repeated_pattern(7), false);
-    assert_eq!(is_repeated_pattern(111), false);
-    assert_eq!(is_repeated_pattern(1234), false);
+    // examples that should return true
+    assert_eq!(
+        is_repeated_pattern(12341234),
+        true,
+        "{} should be repeated pattern",
+        12341234
+    );
+    assert_eq!(
+        is_repeated_pattern(123123123),
+        true,
+        "{} should be repeated pattern",
+        123123123
+    );
+    assert_eq!(
+        is_repeated_pattern(1212121212),
+        true,
+        "{} should be repeated pattern",
+        1212121212
+    );
+    assert_eq!(
+        is_repeated_pattern(1111111),
+        true,
+        "{} should be repeated pattern",
+        1111111
+    );
+    // example data from the puzzle
+    assert_eq!(
+        is_repeated_pattern(11),
+        true,
+        "{} should be repeated pattern",
+        11
+    );
+    assert_eq!(
+        is_repeated_pattern(22),
+        true,
+        "{} should be repeated pattern",
+        22
+    );
+    assert_eq!(
+        is_repeated_pattern(99),
+        true,
+        "{} should be repeated pattern",
+        99
+    );
+    assert_eq!(
+        is_repeated_pattern(111),
+        true,
+        "{} should be repeated pattern",
+        111
+    );
+    assert_eq!(
+        is_repeated_pattern(999),
+        true,
+        "{} should be repeated pattern",
+        999
+    );
+    assert_eq!(
+        is_repeated_pattern(1010),
+        true,
+        "{} should be repeated pattern",
+        1010
+    );
+    assert_eq!(
+        is_repeated_pattern(1188511885),
+        true,
+        "{} should be repeated pattern",
+        1188511885
+    );
+    assert_eq!(
+        is_repeated_pattern(222222),
+        true,
+        "{} should be repeated pattern",
+        222222
+    );
+    assert_eq!(
+        is_repeated_pattern(446446),
+        true,
+        "{} should be repeated pattern",
+        446446
+    );
+    assert_eq!(
+        is_repeated_pattern(38593859),
+        true,
+        "{} should be repeated pattern",
+        38593859
+    );
+    assert_eq!(
+        is_repeated_pattern(565656),
+        true,
+        "{} should be repeated pattern",
+        565656
+    );
+    assert_eq!(
+        is_repeated_pattern(824824824),
+        true,
+        "{} should be repeated pattern",
+        824824824
+    );
+    assert_eq!(
+        is_repeated_pattern(2121212121),
+        true,
+        "{} should be repeated pattern",
+        2121212121
+    );
+
+    // have varying number of digis
+    assert_eq!(
+        is_repeated_pattern(12312312),
+        false,
+        "{} should not be repeated pattern",
+        12312312
+    );
+    assert_eq!(
+        is_repeated_pattern(1234123),
+        false,
+        "{} should not be repeated pattern",
+        1234123
+    );
+    assert_eq!(
+        is_repeated_pattern(121212121),
+        false,
+        "{} should not be repeated pattern",
+        121212121
+    );
+
+    // have each digit exactly once
+    assert_eq!(
+        is_repeated_pattern(123456),
+        false,
+        "{} should not be repeated pattern",
+        123456
+    );
+    assert_eq!(
+        is_repeated_pattern(1698523),
+        false,
+        "{} should not be repeated pattern",
+        1698523
+    );
+    // have same amout of digits but do not repeat the same pattern
+    assert_eq!(
+        is_repeated_pattern(123321),
+        false,
+        "{} should not be repeated pattern",
+        123321
+    );
+    assert_eq!(
+        is_repeated_pattern(12343412),
+        false,
+        "{} should not be repeated pattern",
+        12343412
+    );
+    assert_eq!(
+        is_repeated_pattern(67696976),
+        false,
+        "{} should not be repeated pattern",
+        67696976
+    );
 }
 
 #[test]
@@ -45,10 +208,11 @@ fn test_part1_data() {
     assert_eq!(part1(&puzzle_input), 40214376723);
 }
 
-// #[test]
-// fn test_part2_example() {
-//     panic!("not implemented");
-// }
+#[test]
+fn test_part2_example() {
+    let puzzle_input = parse_input(EXAMPLE_INPUT);
+    assert_eq!(part2(&puzzle_input), 4174379265);
+}
 
 // #[test]
 // fn test_part2_data() {
